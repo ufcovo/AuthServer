a7b50392b85f7d2cb9f27a6aa5d0ff95a05373ce
Mert <79603812+ufcovo@users.noreply.github.com>
2023-09-23 15:53:32 +0300

    Entity Configuration

--- AuthServer.Data/AppDbContext.cs
+++ AuthServer.Data/AppDbContext.cs
@@ -15,5 +15,11 @@ namespace AuthServer.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
         public DbSet<Product> Products { get; set; }
         public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            base.OnModelCreating(builder);
+        }
     }
 }
