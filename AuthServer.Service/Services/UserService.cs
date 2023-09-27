109b0c41e56ae1247bead93c51b5189a4d6ec101
Mert <79603812+ufcovo@users.noreply.github.com>
2023-09-27 21:10:12 +0300

    UserService

---
+++ AuthServer.Service/Services/UserService.cs
@@ -0,0 +1,54 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class UserService : IUserService
+    {
+        #region Constructor
+        private readonly UserManager<UserApp> _userManager;
+
+        public UserService(UserManager<UserApp> userManager)
+        {
+            _userManager = userManager;
+        }
+        #endregion
+
+        #region CreateUserAsync
+        public async Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto)
+        {
+            var user = new UserApp 
+            { 
+                Email = createUserDto.Email,
+                UserName = createUserDto.UserName,
+            };
+            var result = await _userManager.CreateAsync(user, createUserDto.Password);
+            if(!result.Succeeded)
+            {
+                var errors = result.Errors.Select(r => r.Description).ToList();
+                return Response<UserAppDto>.Fail(new ErrorDto(errors, true), StatusCodes.Status400BadRequest);
+            }
+            return Response<UserAppDto>.Success(ObjectMapper.Mapper.Map<UserAppDto>(user), StatusCodes.Status200OK);
+        }
+        #endregion
+
+        #region GetUserByNameAsync
+        public async Task<Response<UserAppDto>> GetUserByNameAsync(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user is null) return Response<UserAppDto>.Fail("Username not found", StatusCodes.Status404NotFound, true);
+            return Response<UserAppDto>.Success(ObjectMapper.Mapper.Map<UserAppDto>(user), StatusCodes.Status200OK);
+        }
+        #endregion
+    }
+}
